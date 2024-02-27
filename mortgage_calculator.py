70bd2c488bc734ae610c4aa86f6ba5b4e6371e03
Alex J <alexj@alexs-air.lan>
2024-02-27 14:19:08 -0600

    Complete introduction to Mortgage Calculator

---
+++ mortgage_calculator.py
@@ -0,0 +1,3 @@
+print("Welcome to Mortgage Calculator")
+#Mortgage Calculator. Will calculate PITI as well as total interest and principal paid for the entirety of the loan. An estimate of montly T&I can also be calculated.
+#inputs would be interest rate, loan amount, term, tax amount, insurane amount, and PMI amount.
