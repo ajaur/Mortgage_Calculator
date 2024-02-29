68aefb6f960096aa08a00594a8235bcc458bf429
Alex J <alexj@alexs-air.lan>
2024-02-29 11:09:28 -0600

    Added T&I calculation in monthly_pi_calculation class. Tested with mortgage_two.

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -4,13 +4,13 @@ print("Welcome to Mortgage Calculator")
 
 print("What would you like to calculate today? (Monthly PI/Total interest and principal paid for life of loan)")
 class Terms:
-    def __init__(self, input_interest_rate_percentage, input_term_in_years, input_loan_amount, input_total_tax_amount = 0, input_insurance_premium = 0, input_monthly_PMI_amount = 0):
+    def __init__(self, input_interest_rate_percentage, input_term_in_years, input_loan_amount, input_total_tax_amount = 0, input_insurance_premium = 0, input_monthly_pmi_amount = 0):
         self.interest_rate = input_interest_rate_percentage
         self.term = input_term_in_years
         self.loan_amount = input_loan_amount
         self.taxes = input_total_tax_amount
         self.insurance = input_insurance_premium
-        self.PMI = input_monthly_PMI_amount
+        self.pmi = input_monthly_pmi_amount
 
         
 class Monthly_PI_Calculation:
@@ -20,7 +20,15 @@ class Monthly_PI_Calculation:
     def calculation(self, terms):
         monthly_pi = ((terms.interest_rate / 100 / 12) * terms.loan_amount) / (1 - ((1 + (terms.interest_rate / 100 / 12)) ** (-(terms.term) * 12)))
         rounded_monthly_pi = round(monthly_pi, 2)
-        print("The monthly Principal and Interest Payment for this loan is ", rounded_monthly_pi)
+        monthly_ti = terms.taxes / 12 + terms.insurance / 12 + terms.pmi
+        rounded_monthly_ti = round(monthly_ti, 2)
+        piti = round(rounded_monthly_pi + rounded_monthly_ti, 2)
+        print("The monthly Principal and Interest Payment for this loan is", rounded_monthly_pi)
+        if monthly_ti == 0:
+            pass
+        else:
+            print("Your estimated monthly Tax and Insurance payment is", rounded_monthly_ti, "Your total estimated monthly PITI payment is", piti)
+        
 
 
 
@@ -33,4 +41,6 @@ mortgage_two = Terms(3.7, 20, 450000, 10000, 2500, 110)
 
 pi_calculation = Monthly_PI_Calculation()
 
-pi_calculation.calculation(mortgage_one)
+#pi_calculation.calculation(mortgage_one)
+
+pi_calculation.calculation(mortgage_two)
